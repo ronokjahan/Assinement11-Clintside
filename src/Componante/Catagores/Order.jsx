d8be0b9f5bd856c35826ddfc4729624cfb8599e9
ronokjahan <41621327+ronokjahan@users.noreply.github.com>
2023-12-20 19:08:18 +0600

    Update Order.jsx

--- src/Componante/Catagores/Order.jsx
+++ src/Componante/Catagores/Order.jsx
@@ -19,7 +19,7 @@ const Order = () => {
         
         }
         console.log(all);
-        fetch('http://localhost:3000/order',{
+        fetch('https://assinement111-serverside.vercel.app/user',{
           method:'POST',
           headers:{
             "Content-Type": "application/json"
